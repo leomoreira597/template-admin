5842e6b64603b2dff76821e279f2ec4cee2ed8d7
Leonardo Moreira <lmoreira597@gmail.com>
2022-09-23 11:43:40 -0300

    começando a implementar context API

---
+++ data/context/AppContext.tsx
@@ -0,0 +1,21 @@
+import { createContext } from "react";
+
+const AppContext = createContext({
+    nome: null
+})
+
+export function AppProvider(props){
+    return (
+        <AppContext.Provider value={{
+            nome: 'Teste Context Api'
+        }}>
+
+            {props.children}
+
+        </AppContext.Provider>
+    );
+}
+
+export default AppContext
+
+export const AppConsumer = AppContext.Consumer
--- pages/notificacoes.tsx
+++ pages/notificacoes.tsx
@@ -1,9 +1,13 @@
 import Layout from "../components/template/Layout";
+import useAppData from "../data/hooks/useAppData";
 
 export default function Notificacoes(props){
+
+    const dados = useAppData()
+
     return(
         <Layout titulo="Notificações" subtitulo="Aqui será as notificações">
-            <h3>Notificações</h3>
+            <h3>{dados.nome}</h3>
         </Layout>
     );
 }
