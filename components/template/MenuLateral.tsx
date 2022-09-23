6db869717e26f2203bdcb05e051bea37fab49f09
Leonardo Moreira <lmoreira597@gmail.com>
2022-09-23 11:00:54 -0300

    Alterações do menu lateral

--- components/template/MenuLateral.tsx
+++ components/template/MenuLateral.tsx
@@ -1,13 +1,23 @@
 import MenuItem from "./MenuItem";
-import { IconeCasa, IconeAjustes, IconeSino } from "../icons";
+import { IconeCasa, IconeAjustes, IconeSino, IconeSair } from "../icons";
+import Logo from "./Logo";
 
 export default function MenuLateral(){
     return(
-        <aside>
-            <ul>
+        <aside className={`flex flex-col `}>
+            <div className={`flex flex-col items-center justify-center 
+            h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800`
+            }>
+                <Logo />
+            </div>
+            <ul className={` flex-grow`}>
                 <MenuItem url="/" texto="Inicio" icone={IconeCasa}  />
                 <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
                 <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
+            </ul>
+            <ul>
+                <MenuItem onClick={() => console.log('Saiu')} texto="Sair" icone={IconeSair} 
+                className={`text-red-600 hover:bg-red-400 hover:text-white`} />
             </ul>
         </aside>
     );
